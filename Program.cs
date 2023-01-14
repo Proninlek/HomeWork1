7a06f128d15867db965e6ca4794824353df75549
DFIT Vasia <pronin_lek@mail.ru>
2023-01-14 15:58:51 +0300

    Practic 21

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//В условии задачи ничего не сказано про знак равенство, поэтому предположим следующее решение
+
+Console.WriteLine("Введите первое число: ");
+int number1 = int.Parse (Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse (Console.ReadLine());
+if(number1 > number2)
+{
+    Console.WriteLine($"Число {number1} больше числа {number2}");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"Число {number1} меньше числа {number2}");
+}
+else
+{
+    Console.WriteLine("Числа равны");
+}
